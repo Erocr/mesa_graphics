f8bff11a3ab51b758af3a07a774833c74ba8aa8d
Niccolo Angeli <niccolo.angeli@universite-paris-saclay.fr>
2026-06-05 14:42:12 +0200

    feature: Affichage de certains elements d'UI

---
+++ mesa_graphics/UIElement.py
@@ -0,0 +1,43 @@
+import pygame as pg
+
+
+class UIElement:
+    def __init__(self, pos):
+        self.pos = pos
+
+    def draw(self, screen):
+        assert False, "this is an abstract method"
+
+
+class Rectangle(UIElement):
+    def __init__(self, pos, size, color):
+        super().__init__(pos)
+        self.size = size
+        self.color = color
+
+    def draw(self, screen):
+        pg.draw.rect(screen, self.color, pg.Rect(self.pos, self.size))
+
+
+class Text(UIElement):
+    def __init__(self, pos, text, font_size=32):
+        super().__init__(pos)
+        font = pg.font.Font('freesansbold.ttf', font_size)
+        self.image = font.render(text, False, (0, 0, 0))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.pos)
+
+
+class UIButton(UIElement):
+    def __init__(self, pos, text: str, font_size=32):
+        super().__init__(pos)
+        self.text = Text(pos+pg.Vector2(10, 10), text, font_size)
+        self.size = pg.Vector2(self.text.image.get_size()) + pg.Vector2(20, 20)
+        self.hover = False
+
+    def draw(self, screen):
+        bg_color = (220, 220, 220) if self.hover else (180, 180, 180)
+        pg.draw.rect(screen, bg_color, pg.Rect(self.pos, self.size))
+        self.text.draw(screen)
+
--- mesa_graphics/View.py
+++ mesa_graphics/View.py
@@ -1,13 +1,39 @@
-import pygame as pg
+from mesa_graphics.UIElement import *
 
 
 class View:
-    def __init__(self, model):
+    def __init__(self, model, name=None):
         pg.font.init()
-        self.screen = pg.display.set_mode((1000, 700), pg.RESIZABLE)
+        self.screen = pg.display.set_mode((1280, 740), pg.RESIZABLE)
         self.model = model
+        self.name = name
+        if name is None:
+            self.name = type(self.model).__name__
+        self.ui_elements = []
+        self.create_ui()
+
+    def create_ui(self):
+        self.ui_elements += self._up_bar() + self._controls()
+
+    def _up_bar(self):
+        return [
+            Rectangle(pg.Vector2(0, 0), pg.Vector2(1280, 80), (0, 80, 255)),
+            Text(pg.Vector2(80, 0), self.name)
+        ]
+
+    def _controls(self):
+        res = [
+            Rectangle(pg.Vector2(0, 80), pg.Vector2(300, 660), (220, 220, 220)),
+            Text(pg.Vector2(20, 80), "Controls")
+        ]
+        x = 20
+        for text in ("RESET", "▶", "STEP"):
+            res.append(UIButton(pg.Vector2(x, 120), text, font_size=15))
+            x += res[-1].text.image.get_width() + 30
+        return res
 
     def draw(self):
         self.screen.fill((255, 255, 255))
-
+        for ui in self.ui_elements:
+            ui.draw(self.screen)
         pg.display.flip()
