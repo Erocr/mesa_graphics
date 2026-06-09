349bb51d95d34490b4307b0b6a647d4925bd228f
Niccolo Angeli <niccolo.angeli@universite-paris-saclay.fr>
2026-06-09 10:28:03 +0200

    test: Changed test for parameters

--- tests/with_model_params.py
+++ tests/with_model_params.py
@@ -1,41 +1,78 @@
-from mesa_graphics.MesaGraphics import MesaGraphics
-
 import mesa
+from mesa.discrete_space import CellAgent
+from mesa.visualization.components import AgentPortrayalStyle
+from mesa.visualization.space_renderer import SpaceRenderer
+
+from mesa_graphics.MesaGraphics import MesaGraphics
+from mesa_graphics.matplotlib_components import make_mpl_plot_component
 
 
-class MoneyAgent(mesa.Agent):
-    def __init__(self, model):
+class MoneyAgent(CellAgent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, model, cell):
         super().__init__(model)
+        self.cell = cell
+        self.wealth = 10
 
     def step(self):
-        pass
+        cell = self.random.choice(self.cell.neighborhood.cells)
+        self.move_to(cell)
+        agent = self.random.choice(self.model.agents)
+        exchange = self.wealth // 2
+        agent.wealth += exchange
+        self.wealth -= exchange
+
+
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.agents]
+    x = sorted(agent_wealths)
+    n = model.num_agents
+    B = sum(xi * (n - i) for i, xi in enumerate(x)) / (n * sum(x))
+    return 1 + (1 / n) - 2 * B
 
 
 class MoneyModel(mesa.Model):
-    def __init__(self, n=5, seed=None):
+    def __init__(self, n=10, seed=None):
         super().__init__(seed=seed)
-        MoneyAgent.create_agents(model=self, n=n)
-
-        self.datacollector = mesa.DataCollector(model_reporters={}, agent_reporters={})
+        self.num_agents = n
+        self.datacollector = mesa.DataCollector(model_reporters={"Gini": compute_gini},
+                                                agent_reporters={"Wealth": "wealth"})
+        self.grid = mesa.discrete_space.HexGrid((6, 6), torus=True, random=self.random)
+        choices = self.random.choices(self.grid.all_cells.cells, k=self.num_agents)
+        MoneyAgent.create_agents(self, n, choices)
+        self.datacollector.collect(self)
 
     def step(self):
         self.datacollector.collect(self)
         self.agents.shuffle_do("step")
 
 
-model = MoneyModel()
+money_model = MoneyModel()
+
+
+def agent_portrayal(agent):
+    return AgentPortrayalStyle(color="tab:orange", size=50)
+
+
+renderer = SpaceRenderer(model=money_model, backend="matplotlib")
+renderer.setup_agents(agent_portrayal)
+renderer.render()
+
+GiniPlot = make_mpl_plot_component("Gini", page=0)
+
 
 model_params = {
     "n": {
         "type": "SliderInt",
         "value": 5,
         "label": "Number of agents:",
-        "min": 0,
-        "max": 10,
+        "min": 1,
+        "max": 40,
         "step": 1,
     },
     "seed": {
-        "type": "SliderInt",
+        "type": "SliderFloat",
         "value": 1,
         "min": 0,
         "max": 100,
@@ -43,4 +80,10 @@ model_params = {
     }
 }
 
-MesaGraphics(model, model_params=model_params)
+page = MesaGraphics(
+    money_model,
+    renderer,
+    model_params=model_params,
+    components=[GiniPlot],
+)
+
