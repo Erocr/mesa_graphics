b64b36e2f3c7ac47327a58cfb1015b87dd842e9b
Niccolo Angeli <niccolo.angeli@universite-paris-saclay.fr>
2026-07-28 12:08:02 +0200

    feature: improvement A* speed

--- tests/taxis_simulation/a_star_algorithm.py
+++ tests/taxis_simulation/a_star_algorithm.py
@@ -9,9 +9,14 @@ class AStarNode:
         self.cost = cost
         self.heuristic = heuristic
         self.cell = cell
+        self.parent: mesa.discrete_space.Cell | None = None
 
     def __lt__(self, other):
-        return self.heuristic < other.heuristic
+        """
+        Surcharge l'opérateur <
+        Utile pour heapq
+        """
+        return self.heuristic - self.cost < other.heuristic - other.cost
 
 
 def a_star(cell1: mesa.discrete_space.Cell, cell2: mesa.discrete_space.Cell):
@@ -19,16 +24,16 @@ def a_star(cell1: mesa.discrete_space.Cell, cell2: mesa.discrete_space.Cell):
     Prend en paramètre les positions de deux points p1 et p2.
     Il renvoie le chemin pour aller de p1 à p2 trouvé utilisant l'algorithme A*
     """
-    # closed_list associe aux cases déjà visitées le coût pour aller à cette case
-    closed_list: dict[AStarNode: int] = {}
+    # closed_list est un dictionnaire qui associe aux noeuds déjà visités le noeud parent.
+    closed_list: dict[mesa.discrete_space.Cell: mesa.discrete_space.Cell] = {}
 
     # open_list contient les cases à visiter
-    # C'est une file prioritaire, on va gérer ça avec heapq
+    # C'est une file prioritaire, on gère ça avec heapq
     open_list: list[AStarNode] = []
-    cost = 0  # Le coût pour aller de p1 au point initial
-    heuristic = cost + dist(cell1.position, cell2.position)
-    heapq.heappush(open_list, AStarNode(cost, heuristic, cell1))
-    # On met heuristique d'abord, car heapq utilise le premier élément du tuple pour faire ses comparaisons
+
+    # On ajoute l'élément de départ.
+    # Il a un coût de 0, et l'heuristique est la distance à la case d'arrivée
+    heapq.heappush(open_list, AStarNode(0, dist(cell1.position, cell2.position), cell1))
 
     start = time.time()
     # TODO: ne prend pas en compte les directions
@@ -37,8 +42,8 @@ def a_star(cell1: mesa.discrete_space.Cell, cell2: mesa.discrete_space.Cell):
 
         # Si c'est la case d'arrivée, on a fini
         if u_node.cell.position[0] == cell2.position[0] and u_node.cell.position[1] == cell2.position[1]:
-            print(f"found in {time.time() - start}s")
-            closed_list[u_node.cell] = u_node.cost
+            print(f"found in {time.time() - start}s visiting {len(closed_list)} cells")
+            closed_list[u_node.cell] = u_node.parent
             return reconstruct_path(closed_list, cell1, cell2)
 
         # On ajoute les voisins dans open_list
@@ -48,39 +53,39 @@ def a_star(cell1: mesa.discrete_space.Cell, cell2: mesa.discrete_space.Cell):
                 continue
 
             v_cost = u_node.cost + 1  # TODO: améliorer le coût pour qu'il prenne e compte la décélération quand on tourne
+
+            # Cherche si v est déjà dans open_list.
+            # Si c'est le cas, on met le noeud associé dans v_in_open_list
             v_in_open_list = search_cell(open_list, v)
 
-            # Si v est dans open_list avec un coût plus bas, alors il y a un chemin plus rapide qui va jusqu'à ce
-            # noeud : on choisit ce chemin, pas le nouveau
+            # On choisit ce chemin seulement s'il n'y a pas déjà un meilleur chemin
             if v_in_open_list is None or v_in_open_list.cost > v_cost:
-                # On calcule le noeud
+                if v_in_open_list is not None:
+                    # On détruit le noeud déjà existant
+                    open_list.remove(v_in_open_list)
+                    heapq.heapify(open_list)
+
+                # On calcule le noeud à ajouter
                 v_heuristic = v_cost + dist(cell2.position, v.position)
                 v_node = AStarNode(v_cost, v_heuristic, v)
+                v_node.parent = u_node.cell
 
                 # On ajoute le noeud
                 heapq.heappush(open_list, v_node)
 
         # On ajoute dans closed_list la case qu'on vient de visiter
-        closed_list[u_node.cell] = u_node.cost
+        closed_list[u_node.cell] = u_node.parent
 
     raise RuntimeError(f"There is no path from {cell1.position} to {cell2.position}")
 
 
 def reconstruct_path(closed_list, cell1, cell2):
     """ Reconstruit le chemin qui va de cell1 à cell2 par rapport au closed_list """
-    # Insert à la première place est plus long qu'à la dernière.
-    # Donc, on le crée à l'envers, et on retourne la liste à la fin.
+    # Dans closed_list, on associe les parents des cellules.
+    # Donc, on retrouve les parents jusqu'à trouver cell1
     inverted_path = [cell2]
     while inverted_path[-1] != cell1:
-        current_cell = inverted_path[-1]
-        for v in current_cell.neighborhood:
-            if v in closed_list and closed_list[v] == closed_list[current_cell] - 1:
-                inverted_path.append(v)
-                break
-        # S'il n'y a pas eu de break dans la boucle for
-        else:
-            inverted_path.reverse()
-            raise RuntimeError(f"Could not reconstruct the path. Path found so far : {inverted_path}")
+        inverted_path.append(closed_list[inverted_path[-1]])
 
     inverted_path.reverse()
     return inverted_path
@@ -88,15 +93,17 @@ def reconstruct_path(closed_list, cell1, cell2):
 
 def search_cell(open_list, searched_cell) -> AStarNode | None:
     """
-    Cherche une cellule dans open_list.
+    Cherche une cellule dans open_list, s'il y en a plusieurs identiques, donne celle avec le coût minimal.
     S'il la trouve, renvoie le noeud associé, sinon renvoie None
     """
     for node in open_list:
         if node.cell == searched_cell:
             return node
-    return None
 
 
 def dist(p1, p2):
-    """ Calcule la distance euclidienne entre p1 et p2 """
-    return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+    """
+    Calcule la distance infinie entre p1 et p2.
+    On choisit la distance infinie car la grille est de Von Neumann (4 cases adjacentes)
+    """
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
